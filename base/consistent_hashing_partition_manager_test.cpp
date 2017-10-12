dde13ae2ab0b9339f363ac593612e456710da95e
Benjamin Wong <tkwong@inspiresynergy.com>
2017-10-12 16:54:38 +0800

    refs #2, implement the Slice(Keys) with the format 0-> [2,8], 1->[9]

--- base/consistent_hashing_partition_manager_test.cpp
+++ base/consistent_hashing_partition_manager_test.cpp
@@ -42,12 +42,23 @@ TEST_F(TestConsistentHashingPartitionManager, GetServerThreadIds) {
 TEST_F(TestConsistentHashingPartitionManager, SliceKeys) {
 
     using Keys = AbstractPartitionManager::Keys;
-    Keys in_keys({1,2,3,4,5,6,7,8,9,10,11,12,13,14,15});
+    Keys in_keys({2,8,9});
     std::vector<std::pair<int, Keys>> sliced;
 
     // TEST with keys 1,2,3,4,5 and not empty
     pm->Slice(in_keys, &sliced);
     ASSERT_FALSE(sliced.empty());
+	
+    // ASSERT_EQ(sliced.size(), 3);            // 2 slices for 2 servers
+    EXPECT_EQ(sliced[0].first, 0);          // key to server 0
+    ASSERT_EQ(sliced[0].second.size(), 2);  // key 2, 8
+    EXPECT_EQ(sliced[0].second[0], 2);
+    EXPECT_EQ(sliced[0].second[1], 8);
+	
+    EXPECT_EQ(sliced[1].first, 1);          // keys to server 1
+    ASSERT_EQ(sliced[1].second.size(), 1);  // keys 9
+    EXPECT_EQ(sliced[1].second[0], 9);
+
 }
 
 TEST_F(TestConsistentHashingPartitionManager, SliceKeysIfNodeDeleted) {GTEST_FAIL();}
