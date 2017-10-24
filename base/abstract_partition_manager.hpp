cc23f82ef63026983fa621510c94897e18fae8f6
Benjamin Wong <tkwong@inspiresynergy.com>
2017-10-24 23:31:11 +0800

    reformat abstract partition manager to follow scripts/lint.py

--- base/abstract_partition_manager.hpp
+++ base/abstract_partition_manager.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cinttypes>
+#include <utility>
 #include <vector>
 
 #include "base/magic.hpp"
@@ -9,26 +10,32 @@
 namespace csci5570 {
 
 /*
- * Implments the interface of a PartitionManager which provides the model partitioning scheme
+ * Implments the interface of a PartitionManager which provides the model
+ * partitioning scheme
  */
 class AbstractPartitionManager {
- public:
+public:
   // using Keys = std::vector<Key>;
   // using KVPairs = std::pair<std::vector<Key>, std::vector<double>>;
   using Keys = third_party::SArray<Key>;
-  using KVPairs = std::pair<third_party::SArray<Key>, third_party::SArray<double>>;
+  using KVPairs =
+      std::pair<third_party::SArray<Key>, third_party::SArray<double>>;
 
-  AbstractPartitionManager(const std::vector<uint32_t>& server_thread_ids) : server_thread_ids_(server_thread_ids) {}
+  explicit AbstractPartitionManager(
+      const std::vector<uint32_t> &server_thread_ids)
+      : server_thread_ids_(server_thread_ids) {}
 
   size_t GetNumServers() const;
-  const std::vector<uint32_t>& GetServerThreadIds() const;
+  const std::vector<uint32_t> &GetServerThreadIds() const;
 
   // slice keys into <server_id, key_partition> pairs
-  virtual void Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) const = 0;
+  virtual void Slice(const Keys &keys,
+                     std::vector<std::pair<int, Keys>> *sliced) const = 0;
   // slice key-value pairs into <server_id, key_value_partition> pairs
-  virtual void Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) const = 0;
+  virtual void Slice(const KVPairs &kvs,
+                     std::vector<std::pair<int, KVPairs>> *sliced) const = 0;
 
- protected:
+protected:
   std::vector<uint32_t> server_thread_ids_;
 };  // class AbstractPartitionManager
 
