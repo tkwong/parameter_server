823414e6b0177e08aefaab899d8c3375dd3a0640
Benjamin Wong <tkwong@inspiresynergy.com>
2017-10-27 14:51:32 +0800

    fix the compilation problem of libhdfs3

---
+++ lib/async_read_buffer_test.cpp
@@ -0,0 +1,41 @@
+#include "glog/logging.h"
+#include "gtest/gtest.h"
+#include "boost/utility/string_ref.hpp"
+
+#include "lib/abstract_async_data_loader.hpp"
+
+namespace csci5570 {
+namespace lib {
+namespace {
+
+class TestAsyncReadBuffer : public testing::Test {
+    public:
+        TestAsyncReadBuffer() {}
+        ~TestAsyncReadBuffer() {}
+    
+    protected:
+        void SetUp() {}
+        void TearDown() {}
+};
+
+TEST_F(TestAsyncReadBuffer, DISABLED_init) {
+    
+}
+
+TEST_F(TestAsyncReadBuffer, DISABLED_get_batch) {
+    
+}
+
+TEST_F(TestAsyncReadBuffer, ask) {
+    AsyncReadBuffer buffer_;
+    // buffer_.init("http://localhost/", 0,1,1,1);
+    // EXPECT_EQ(buffer_.ask(), 1);
+}
+
+TEST_F(TestAsyncReadBuffer, end_of_file) {
+    
+}
+
+} // namespace
+} // namespace lib
+} // namespace csci5570
